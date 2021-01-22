6ccb944f50a36a6f90676290b89a25eb963acf64
Vadim Brik <vadimbrik@icloud.com>
2021-01-22 14:57:34 +0600

    Initial Commit

---
+++ news-app/news_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_appApp.swift
+//  news-app
+//
+//  Created by Vadim Brik on 22.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct news_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
